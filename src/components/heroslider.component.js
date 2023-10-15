f2447108e83c11827126517467e23e05210d0ef0
Sazad Ahemad <mail2sazad@gmail.com>
2023-10-15 23:50:49 +0530

    added heroslider

--- src/components/heroslider.component.js
+++ src/components/heroslider.component.js
@@ -1,60 +1,95 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const HeroSlider = () => {
+// swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, EffectFade } from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
 
-    return (
-        <section className="slider_section long_section">
-            {/* <div id="customCarousel" className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className="container ">
-                            <div className="row">
-                                <div className="col-md-5">
-                                    <div className="detail-box">
-                                        <h1>
-                                            For All Your <br />
-                                            Furniture Needs
-                                        </h1>
-                                        <p>
-                                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
-                                        </p>
-                                        <div className="btn-box">
-                                            <Link to="" className="btn1">
-                                                Contact Us
-                                            </Link>
-                                            <Link to="" className="btn2">
-                                                About Us
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-7">
-                                    <div className="img-box">
-                                        <img src="images/slider-img.png" alt="" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+class HeroSlider extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            slides: [
+                {
+                    title: "For All Your Furniture Needs",
+                    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.",
+                    btn1_name: "Contact Us",
+                    btn1_link: "#",
+                    btn2_name: "About Us",
+                    btn2_link: "#",
+                }
+            ],
+            test: "h"
+        }
+    }
+
+    componentDidMount() {
+        // Move your side effect code here
+    }
+
+    render() {
+
+        return (
+            <section className="slider_section long_section">
+                <div id="customCarousel" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <Swiper
+                            className="mySwiper"
+                            loop={true}
+                            pagination={{ dynamicBullets: true }}
+                            modules={[Pagination, Navigation, EffectFade]}
+                            effect={"fade"}
+                        >
+                            <SwiperSlide><Slide no={1} heading={this.state.slides[0].title}/></SwiperSlide>
+                            <SwiperSlide><Slide no={2} heading={this.state.slides[0].title}/></SwiperSlide>
+                            <SwiperSlide><Slide no={3} heading={this.state.slides[0].title}/></SwiperSlide>
+                        </Swiper>
                     </div>
-                    
-                    <Slide/>
                 </div>
-                <ol className="carousel-indicators">
-                    <li data-target="#customCarousel" data-slide-to="0" className="active"></li>
-                    <li data-target="#customCarousel" data-slide-to="1"></li>
-                    <li data-target="#customCarousel" data-slide-to="2"></li>
-                </ol>
-            </div> */}
-            <Slide />
-        </section>
-    );
+            </section >
+        );
+    }
 }
 
 export default HeroSlider;
 
-const Slide = () => {
+const Slide = ({ no, heading }) => {
     return (
         <>
+            <div className="carousel-item active">
+                <div className="container ">
+                    <div className="row">
+                        <div className="col-md-5">
+                            <div className="detail-box">
+                                <h1>
+                                    {heading}
+                                </h1>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo maxime voluptatem a itaque suscipit.
+                                </p>
+                                <div className="btn-box">
+                                    <Link to="#" className="btn1">
+                                        Contact Us
+                                    </Link>
+                                    <Link to="#" className="btn2">
+                                        About Us
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-7">
+                            <div className="img-box">
+                                <img src={process.env.PUBLIC_URL + "assets/images/slider-img.png"} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
